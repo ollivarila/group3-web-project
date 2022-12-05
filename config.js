7af57574d2fb72102a07cf7a6db1f1c17226d03b
ollivarila <olli.varila@gmail.com>
2022-12-05 21:02:18 +0200

    config

---
+++ config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
+module.exports = {
+  MONGODB_URI,
+}
