9ab14f109d3dda50903e2619bfd8fd72d3a84a4d
ollivarila <olli.varila@gmail.com>
2022-12-05 21:20:44 +0200

    Website ready for development

---
+++ website/src/index.js
@@ -0,0 +1,11 @@
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+)
