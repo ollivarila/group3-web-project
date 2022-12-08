e195dd97dde7c8b6970c53bf1c3d74fa02a11085
ollivarila <olli.varila@gmail.com>
2022-12-08 15:57:36 +0200

    Added correct cache path

--- config/paths.js
+++ config/paths.js
@@ -66,7 +66,7 @@ module.exports = {
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
-  appWebpackCache: resolveApp('node_modules/.cache'),
+  appWebpackCache: resolveApp('../node_modules/.cache'),
   appTsBuildInfoFile: resolveApp('node_modules/.cache/tsconfig.tsbuildinfo'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
   publicUrlOrPath,
