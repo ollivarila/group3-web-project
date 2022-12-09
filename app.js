3bae968f30729c0d4d55a37121745794a068beb3
ollivarila <olli.varila@gmail.com>
2022-12-09 22:44:29 +0200

    Router stuff, Home file and static path to backend for later

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const path = require('path')
 const startUp = require('./utils/startUp')
 const loginRouter = require('./server/routes/user')
 const shoppingListRouter = require('./server/routes/shoppingList')
 
-dotenv.config();
+dotenv.config()
 
 startUp()
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
 app.use(express.json())
 
+// app.use(express.static('build'))
+
 app.use('/api/user', loginRouter)
 app.use('/api/shoppingLists', shoppingListRouter)
-
-app.get('/', (req, res) => {
-  res.send({ message: 'hello world!' });
-});
-
-const port = process.env.PORT || 3000;
+/*
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, 'build/'),
+  })
+})
+*/
+const port = process.env.PORT || 3000
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => console.log(`Server listening on ${port}`));
+  app.listen(port, () => console.log(`Server listening on ${port}`))
 }
 
 module.exports = app
--- website/src/App.js
+++ website/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Signup from './pages/Signup/Signup'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
     <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<div>404 not found</div>} />
+      </Routes>
     </div>
   )
 }
