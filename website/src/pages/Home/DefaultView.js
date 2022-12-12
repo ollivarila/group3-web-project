b28bc4551dbc1f6b143b8be30f93433ae94bc750
Jenni Harakka <jvharakka@gmail.com>
2022-12-12 13:36:44 +0200

    sidebar + defaultview

--- website/src/pages/Home/DefaultView.js
+++ website/src/pages/Home/DefaultView.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React , {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { initializeUser} from '../../reducers/userReducer'
+import '../../components/styles/defaultviewstyle.css'
+
 const DefaultView = () => {
- 
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    useEffect(() => {
+      dispatch(initializeUser())
+    }, [dispatch])
 return (
-    <>
-    <h1>Welcome, </h1>
-    <p>To add a new list to your shopping list, click "+ Add a new list"</p>
-    <p>To see and edit your shopping lists, click on any of your existing lists</p>
-    </>
+    <div className='text'>
+    <h1>Welcome, {user.username}</h1> 
+    <p><b>To add a new list</b> to your shopping lists, click "+ Add a new list"</p>
+    <p><b>To see and edit</b> your shopping lists, click on any of your existing lists on the left side of the page</p>
+    </div>
 )
 }
 export default DefaultView
